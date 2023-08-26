126e80482a44578e3e2d9eb93fe8cc76f94846d4
Kishan Utkarsh <59743564+KishanUtkarsh@users.noreply.github.com>
2023-08-26 14:45:01 +0530

    Update JdbcCrudApplication.java
    learning git

--- src/main/java/jpa_crud/JdbcCrudApplication.java
+++ src/main/java/jpa_crud/JdbcCrudApplication.java
@@ -3,6 +3,8 @@ package jpa_crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Changes in git repo
+
 @SpringBootApplication
 public class JdbcCrudApplication {
 
