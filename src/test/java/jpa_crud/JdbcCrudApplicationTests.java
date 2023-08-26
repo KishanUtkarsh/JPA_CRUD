f91604b527d2d0c93d35c8ac153560a2c14299ec
Kishan Utkarsh <59743564+KishanUtkarsh@users.noreply.github.com>
2023-08-26 14:17:46 +0530

    first commit

---
+++ src/test/java/jpa_crud/JdbcCrudApplicationTests.java
@@ -0,0 +1,17 @@
+package jpa_crud;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Fail.fail;
+
+@SpringBootTest
+class JdbcCrudApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+		fail("hello");
+	}
+
+}
